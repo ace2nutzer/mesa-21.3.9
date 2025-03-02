3082cabccec99f75cce49572061e2443153e2076
ace2nutzer <ace2nutzer@gmail.com>
2025-03-02 21:01:44 +0100

    prepare to add dri-sis driver

--- setup.sh
+++ setup.sh
@@ -1,3 +1,3 @@
 #! /bin/sh
 
-meson setup build --reconfigure --prefix $HOME/mesa/install -Ddri3=disabled -Dvulkan-drivers= -Dgallium-drivers=swrast -Ddri-drivers= -Dplatforms=x11 -Degl-native-platform=x11
+meson setup build --reconfigure --prefix $HOME/mesa/install -Dllvm=disabled -Ddri3=disabled -Dvulkan-drivers= -Dgallium-drivers=swrast -Ddri-drivers=sis -Dplatforms=x11 -Degl-native-platform=x11
